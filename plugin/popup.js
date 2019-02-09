1ca1fbe0b4cf7ccc50484d5385e89055c37b7c80
Sathya <sathyahari9@gmail.com>
2019-02-08 21:48:38 -0500

    styles

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,5 +1,4 @@
   let changeColor = document.getElementById('changeColor');
-
   chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
